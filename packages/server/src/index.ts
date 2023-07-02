4ac2ee9618d33604dff6df313254cb0b68503b23
RodrigoSester <rsesterheim@maisaedu.com.br>
2023-07-02 17:48:12 -0400

    feat: get experiences

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,26 @@
-import express from "express";
+import express from 'express';
+import { Pool } from 'pg'
+require('dotenv').config();
+
+const pool = new Pool({
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT!),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+})
 const app = express();
-const port = 3000;
 
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.get('/experiences', async ( req, res ) => {
+    const query = `
+        SELECT * FROM experience
+    `
 
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+    pool.query(query, ( err, { rows } ) => {
+        res.status(200).json(rows);
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log( `server started at http://localhost:${ process.env.PORT }` );
+});
