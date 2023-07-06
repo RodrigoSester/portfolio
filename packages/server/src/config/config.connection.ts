e6da086f544388948fa98cc71667c4f2230b8130
RodrigoSester <rsesterheim@maisaedu.com.br>
2023-07-06 11:55:58 -0400

    feat: implement config connection

---
+++ packages/server/src/config/config.connection.ts
@@ -0,0 +1,13 @@
+import { PoolConfig } from 'pg';
+
+require('dotenv').config();
+
+const config: PoolConfig = {
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT!),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+}
+
+export default config;
