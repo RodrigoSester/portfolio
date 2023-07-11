945324920abbcded8ebc7b558f24e7b0192cbb1a
RodrigoSester <rsesterheim@maisaedu.com.br>
2023-07-10 21:31:07 -0400

    feat: default values

--- packages/server/knexfile.ts
+++ packages/server/knexfile.ts
@@ -6,10 +6,10 @@ require('dotenv').config();
 module.exports = {
   client: 'pg',
   connection: {
-    server: path.join(process.env.DB_DATABASE!),
+    server: path.join(process.env.DB_DATABASE || ''),
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT!),
+    port: parseInt(process.env.DB_PORT || '3000'),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD
   },
