4eb3d49876a59d2e6e571ed6195b9cd873c6a0b3
RodrigoSester <rsesterheim@maisaedu.com.br>
2023-07-10 21:56:17 -0400

    feat: default value

--- packages/server/src/config/config.connection.ts
+++ packages/server/src/config/config.connection.ts
@@ -5,7 +5,7 @@ require('dotenv').config();
 const config: PoolConfig = {
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT!),
+    port: parseInt(process.env.DB_PORT || '3000'),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD
 }
