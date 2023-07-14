a6c5b82a614a7967de6ea644bbe2067cb814aaa8
RodrigoSester <rsesterheim@maisaedu.com.br>
2023-07-13 23:00:02 -0400

    feat: card to upload document

--- packages/web/src/components/certifications/CertificationsList.vue
+++ packages/web/src/components/certifications/CertificationsList.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="q-pa-md">
     <div class="row justify-center q-gutter-sm">
+      <q-card
+        flat
+        bordered
+        class="example-item q-ma-sm row items-center justify-center"
+      >
+        <q-card-section class="row items-center justify-center">
+          <div class="row justify-center items-center">
+            <q-file
+              class="file-uploader"
+              hide-bottom-space
+              borderless
+            >
+              <template v-slot:default>
+                <div>
+                  <q-icon name="mdi-cloud-upload-outline" color="primary" size="48px" />
+                </div>
+              </template>
+            </q-file>
+          </div>
+        </q-card-section>
+      </q-card>
       <q-intersection
         v-for="index in 60"
         :key="index"
@@ -28,5 +49,9 @@
 .example-item {
   height: 300px;
   width: 250px;
+}
+
+.file-uploader{
+  width: min-content;
 }
 </style>
